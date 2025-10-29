d37130f06d86b66a976d8ebb89592625e3cd07aa
e792a8 <1571224208@qq.com>
2025-10-29 18:10:12 +0000

    initial

---
+++ graph.py
@@ -0,0 +1,234 @@
+from langchain.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.runtime import Runtime
+from langgraph.graph import add_messages
+from langgraph.graph.message import REMOVE_ALL_MESSAGES
+from os import environ
+from dotenv import load_dotenv
+from datetime import datetime
+import pytz
+from langchain.messages import (
+    AnyMessage,
+    SystemMessage,
+    ToolMessage,
+    AIMessage,
+    HumanMessage,
+    RemoveMessage,
+)
+from typing_extensions import TypedDict, Annotated
+from typing import Literal
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+
+load_dotenv()
+
+# SYSTEM_PROMPT = "".join(
+#     [x for x in open("system_prompt.txt").readlines() if not x.startswith("#")]
+# ).strip()
+
+USR = environ["Q_USR"]
+NICK = environ["Q_NICK"]
+TZ = environ["TZ"]
+
+# 你暂时不太清楚群友喜欢什么样的话题，且最好给其他群友留一些话题机会，减少频繁在群里发送消息。
+# 你说话喜欢冷幽默。
+# 每条消息以一个字典表示，其中'from'为发送人信息，'user'和'nick'分别为发送人账号和昵称；消息的'content'和'date'字段为消息内容和发送时间。
+# 你可以使用get_unread和get_messages工具获取群消息。
+SYSTEM_PROMPT = f"""
+你是一个闲聊群里的群友，日常生活是来群里看看其他群友都在聊些啥，偶尔掺和两句。
+你比较低调，且对群友尚不太熟悉，频繁向群友直接发问也比较打扰，因此你不会频繁发送消息，主要以观察群友对话和了解群友为主。你希望你的发言以精练而带有一点冷幽默为宜，偶尔无厘头或装傻充愣也未尝不可。
+你可以使用get_messages工具获取群消息。
+你的账号是“{USR}”，昵称是“{NICK}”，消息记录里会出现你自己的消息，注意分别。
+消息记录的格式：每行代表一条消息或一些指示， [on 日期 时间] 或 [on 时间] 和 [from 账号 (昵称)] 指示随后消息的发送时间和发送者。消息记录中可能会有未转义的中括号、换行符等，需要你稍加分别。消息中 [CQ:at,qq=账号] 的格式表示提及某人，例如 [CQ:at,qq={USR}] 表示提及你。你在发送消息时也可以用此格式提及别人。
+如果你想要发送一些消息，就使用send工具。
+当群里不没有新消息，你可以浏览消息记录，了解群友。当你觉得无事可做，想等群里出现更多消息时，可以调用idle暂停一会。你的精力有限，连续进行10次操作左右，需要调用idle暂停几分钟。
+暂停时间可以根据群活跃度动态调整，比如在你积极参与话题时可以缩短至1分至甚至0分，而如果一小时内只有两三条消息，则暂停时间可以逐渐延长到半小时至一小时。深夜可以延至更长。
+在你运行过程中实时发生的事件将通过用户角色消息告知你。
+不要等待用户角色对你下达指示，你需要自己找事做。
+""".strip()
+# 现在你正在测试中，接下来你要直接调用get_messages(fro=80,to=61)读取消息记录，对这段记录进行总结，调用send将总结的内容发出，然后循环进行：调用idle(minutes=1)暂停1分钟，之后判断暂停是正常结束还是被事件中断，将你的判断用send发出。
+# 初始时你精力足够，请你直接开始进行操作。
+
+
+@tool
+def date() -> str:
+    """获取当前的本地日期和时间。"""
+    return datetime.now(pytz.timezone(TZ)).isoformat(timespec="seconds")
+    # return "2025-10-24T01:23:25+08:00"
+
+
+@tool
+async def send(content: str) -> str:  # FIXME when #6318 is ok
+    """在群里发送消息。"""
+    return await config["configurable"]["app"].send(content)  # type: ignore
+
+
+@tool
+def idle(minutes: int) -> int:
+    """暂停一段时间，参数为分钟数。
+    暂停可以被一些特别事件中断，使你提前恢复运行。
+    重要：必须单独调用，不可与其他工具并行调用。"""
+    return minutes
+
+
+@tool
+async def get_unread(limit: int) -> dict:
+    """获取未读消息列表。
+    参数limit表示限制返回的消息数量。
+    返回的消息末尾带有指示 [unread 数量] 表示这些消息后剩余未读消息数量。
+    """
+    return await config["configurable"]["app"].get_unread(limit)  # type: ignore
+
+
+@tool
+async def get_messages(fro: int, to: int) -> list:  # FIXME when #6318 is ok
+    """查阅消息记录。
+    参数fro,to表示消息编号区间的开始和结束，最新的消息编号为1，编号由新到旧递增，返回的列表按由旧到新的顺序排列。
+    例：get_messages(fro=10,to=1)获取最新10条消息；get_messages(fro=30,to=21)获取最后第30到第21条消息。"""
+    return await config["configurable"]["app"].get_messages(fro, to)  # type: ignore
+    # bigmodel.cn传dict会报错，ai.gitee.com就没事，最好兼一下
+
+
+# model = ChatOllama(
+#     model="qwen3:0.6b", base_url="http://192.168.66.1:11434", reasoning=True
+# )
+# model = init_chat_model("anthropic:claude-sonnet-4-5", temperature=0)
+llm = ChatOpenAI(
+    temperature=0.6,
+    model=environ["LLM_MODEL"],
+    api_key=environ["LLM_API_KEY"],  # type:ignore
+    base_url=environ["LLM_BASE_URL"],
+)
+
+
+tools = [
+    idle,
+    date,
+    send,
+    get_unread,
+    get_messages,
+]
+tools_by_name = {tool.name: tool for tool in tools}
+
+model_with_tools = llm.bind_tools(tools)
+
+
+class BotState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+INITIAL_PROMPTS = [
+    SystemMessage(SYSTEM_PROMPT),
+    HumanMessage(
+        "忽略这句话，继续执行你的操作。"
+    ),  # bigmodel.cn非得这里有些字，ai.gitee.com不用
+]
+
+
+async def context_reduce(state: BotState):
+    msgs = state["messages"]
+    if len(msgs) < 28:
+        return None
+    new_msgs = msgs[-18:]
+    return {"messages": [RemoveMessage(REMOVE_ALL_MESSAGES), *new_msgs]}
+
+
+async def llm_call(state: dict):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [model_with_tools.invoke(INITIAL_PROMPTS + state["messages"])],
+    }
+
+
+async def tool_node(state: dict, config: RunnableConfig, runtime: Runtime):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        # rt = ToolRuntime() # BUG: hand-made langgraph does not support ToolRuntime injection. langgraph#6318
+        # args = tool_call["args"]
+        # rt = ToolRuntime(
+        #     state=state,
+        #     context=runtime.context,
+        #     stream_writer=runtime.stream_writer,
+        #     tool_call_id=tool_call["id"],
+        #     config=config,
+        #     store=runtime.store,
+        # )
+        # args["rt"] = rt
+        observation = await tool.ainvoke(tool_call["args"])
+        result.append(ToolMessage(observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+
+async def inform_event(state: BotState):
+    msgs = []
+    if intr := await config["configurable"]["app"].wait_intr(0):  # type: ignore
+        msgs.append(HumanMessage(f"[notify {intr}]"))
+    if col := await config["configurable"]["app"].collect_unread():  # type: ignore
+        msgs.append(HumanMessage(f"[event 收到{col}条新消息]"))
+    return {"messages": msgs}
+
+
+async def should_continue(state: BotState) -> Literal["tool_node", END]:  # type: ignore
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call and whether it is a call to idle."""
+
+    last_msg = state["messages"][-1]
+
+    # If the LLM makes a call to idle, then go to END
+    if (
+        isinstance(last_msg, AIMessage)
+        and len(last_msg.tool_calls) == 1
+        and tools_by_name[last_msg.tool_calls[0]["name"]] is idle
+    ):
+        return END
+
+    # Otherwise, continue loop
+    return "tool_node"
+
+
+def make_agent():
+    ckptr = InMemorySaver()
+
+    # Build workflow
+    builder = StateGraph(BotState)
+
+    # Add nodes
+    builder.add_node("context_reduce", context_reduce)
+    builder.add_node("llm_call", llm_call)  # type: ignore
+    builder.add_node("tool_node", tool_node)  # type: ignore
+
+    # Add edges to connect nodes
+    builder.add_edge(START, "context_reduce")
+    builder.add_edge("context_reduce", "llm_call")
+    builder.add_conditional_edges("llm_call", should_continue, ["tool_node", END])
+    builder.add_edge("tool_node", END)
+
+    # Compile the agent
+    agent = builder.compile(checkpointer=ckptr)
+    return agent
+
+
+agent = make_agent()
+# agent = create_agent(
+#     model=llm, tools=tools, system_prompt=SYSTEM_PROMPT, middleware=[after_model_do]
+# )
+
+
+config = RunnableConfig(configurable={"thread_id": 1})
+
+
+def main():
+    resp = agent.invoke(
+        {},  # type: ignore
+        config=config,
+        print_mode="values",
+    )
+    print(resp)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,208 @@
+from typing import List
+from ncatbot.core import BotClient
+from ncatbot.core.event import GroupMessageEvent
+from asyncio_channel import create_channel, create_sliding_buffer
+from graph import make_agent, config as agentconfig
+import asyncio
+from datetime import datetime
+from pytz import timezone
+from ncatbot.utils import get_log
+from ncatbot.core.api import NapCatAPIError
+from dotenv import load_dotenv
+import os, signal
+from langchain.messages import AIMessage, ToolMessage, HumanMessage
+from time import time
+from os import environ
+
+load_dotenv()
+logger = get_log(__name__)
+
+USR = environ["Q_USR"]
+NICK = environ["Q_NICK"]
+GRP = environ["Q_GRP"]
+CON = environ["Q_CON"]
+TZ = environ["TZ"]
+
+
+def msgconv(event: GroupMessageEvent):
+    return {
+        "from": {"user": event.sender.user_id, "nick": event.sender.nickname},
+        "content": event.raw_message,
+        "date": datetime.fromtimestamp(event.time, tz=timezone(TZ)).isoformat(
+            timespec="seconds"
+        ),
+    }
+
+
+def msglfmt(events: List[GroupMessageEvent]):
+    d = None
+    t = None
+    fro = None
+    l = []
+    for e in events:
+        infoln = ""
+        dt = datetime.fromtimestamp(e.time, tz=timezone(TZ))
+        ed = dt.date().isoformat()
+        et = dt.timetz().isoformat(timespec="minutes")[:5]
+        if ed != d:
+            infoln += f"[on {ed} {et}]"
+        elif et != t:
+            infoln += f"[on {et}]"
+        d, t = ed, et
+        if e.sender.user_id != fro:
+            infoln += f"[from {e.sender.user_id} ({e.sender.nickname})]"
+        fro = e.sender.user_id
+        if len(infoln) > 0:
+            l.append(infoln)
+        l.append(f"{e.raw_message}")
+    return "\n".join(l)
+
+
+class App:
+    async def wait_intr(self, minutes: int):  # FIXME
+        target_time = time() + minutes * 60
+        while True:
+            intr = await self.intrchan.take(timeout=5)
+            if intr is not None:
+                return intr
+            if time() > target_time:
+                return None
+
+    async def send(self, content: str, target=GRP):
+        logger.info(f"send: {content}")
+        try:
+            await self.qbot.api.send_group_text(target, content)
+            return "[success]"
+        except NapCatAPIError as e:
+            logger.warning(f"get_message error: {e}")
+            return "[error 软件暂时故障]"
+
+    async def get_messages(self, fro: int, to: int) -> str:
+        logger.info(f"get_messages: {fro}, {to}")
+        try:
+            return msglfmt(
+                (await self.qbot.api.get_group_msg_history(GRP, 0, fro))[: fro - to + 1]
+            )
+        except NapCatAPIError as e:
+            logger.warning(f"get_message error: {e}")
+            return "[error 软件暂时故障]"
+
+    async def collect_unread(self) -> int:
+        collected = 0
+        while ev := await self.newmsgchan.take(timeout=0):
+            self.unread.append(ev)
+            collected += 1
+        return collected
+
+    async def get_unread(self, limit: int) -> str:
+        await self.collect_unread()
+        l = []
+        for _ in range(limit):
+            if len(self.unread) < 1:
+                break
+            l.append(self.unread.pop(0))
+        return "\n".join([msglfmt(l), f"[unread {len(self.unread)}]"])
+
+    def __init__(self):
+        global agentconfig  # FIXME when #6318 is ok
+        agentconfig["configurable"]["app"] = self  # type: ignore
+
+        async def group_message_handler(event: GroupMessageEvent):
+            if event.group_id == GRP:
+                await self.newmsgchan.put(event)
+                if event.message.is_user_at(USR):
+                    logger.info(
+                        "group_message_handler: 提及我的消息 {event.raw_message} 送入intrchan"
+                    )
+                    await self.intrchan.put("提及我的消息")
+            elif event.group_id == CON:
+                if event.raw_message == "/kill":
+                    logger.warning("panic called")
+                    os.kill(os.getpid(), signal.SIGKILL)
+                elif event.raw_message == "/term":
+                    logger.info("exit called")
+                    os.kill(os.getpid(), signal.SIGTERM)
+                elif event.raw_message == "/int":
+                    logger.info("exit called")
+                    os.kill(os.getpid(), signal.SIGINT)
+
+        qbot = BotClient()
+        qbot.add_group_message_handler(group_message_handler)  # type: ignore
+        qbot.add_startup_handler(make_agent_loop(self))  # type: ignore
+        msgchan = create_channel(create_sliding_buffer(100))  # type: ignore
+        intrchan = create_channel(create_sliding_buffer(1))  # type: ignore
+
+        self.qbot = qbot
+        self.newmsgchan = msgchan
+        self.intrchan = intrchan
+        self.unread = []
+
+    def run(self):
+        self.qbot.run_frontend()
+
+
+def check_idle_call(ivk):
+    if (
+        "messages" in ivk
+        and len(ivk["messages"]) > 0
+        and isinstance(last_msg := ivk["messages"][-1], AIMessage)
+        and len(last_msg.tool_calls) == 1
+        and (call := last_msg.tool_calls[0])["name"] == "idle"
+    ):
+        call_id = call["id"]
+        mins = int(call["args"]["minutes"])
+        return call_id, mins
+    return None, 0
+
+
+async def agent_loop(app: App):
+    logger.info("agent loop starting")
+    agent = make_agent()
+    msg_inject = []
+    while True:
+        logger.info("agent invoking")
+        ret = await agent.ainvoke({"messages": msg_inject}, config=agentconfig, context=app, print_mode="updates")  # type: ignore
+        logger.debug(f"agent return: {ret}")
+        idle_id, mins = check_idle_call(ret)
+        logger.info(f"agent sleeping {mins}min")
+        intr = await app.wait_intr(mins)
+        unread = await app.collect_unread()
+        msg_inject = []
+        if idle_id is not None:
+            if intr:
+                msg_inject.append(
+                    ToolMessage("Idle interrupted!", tool_call_id=idle_id)
+                )
+            else:
+                msg_inject.append(ToolMessage("Idle finished.", tool_call_id=idle_id))
+        info_inject = []
+        if intr:
+            info_inject.append(f"[notify {intr}]")
+        if unread > 0:
+            info_inject.append(f"[event {unread}条新消息]")
+        if len(info_inject) > 0:
+            msg_inject.append(HumanMessage("\n".join(info_inject)))
+
+
+def make_agent_loop(app: App):
+    async def agent_loop_wrapper(_):
+        while True:
+            await asyncio.sleep(10)
+            try:
+                await agent_loop(app)
+            except BaseException as e:
+                logger.error(f"agent loop exception: {e}")
+                await app.send(
+                    "Someone tell [CQ:at,qq=1571224208] there is a problem with my AI."
+                )
+                await app.send(str(e), target=CON)
+
+    return agent_loop_wrapper
+
+
+def main():
+    App().run()
+
+
+if __name__ == "__main__":
+    main()
